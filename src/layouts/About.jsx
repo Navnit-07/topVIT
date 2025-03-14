6e996749e9e18d43574de8890c46210feac5866c
Navnit <178172653+Navnit-07@users.noreply.github.com>
2025-03-14 23:53:39 +0530

    add about page

--- src/layouts/About.jsx
+++ src/layouts/About.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import style from './About.module.css'
 
 function About() {
   return (
-    <div>About</div>
+    <div className={style['container']}>
+    
+        <p>
+            Welcome to topVIT – your ultimate academic companion! <br/>
+
+            We are a team of passionate learners and achievers who believe that success is not just about passing exams but excelling in them. Our platform provides high-quality notes, curated playlists, and expert guidance from 9+ CGPA achievers, ensuring you have everything you need to understand concepts deeply and score top marks.<br/>
+
+            📚 What We Offer?<br/>
+            ✔️ High-Quality Notes – Well-structured and concise notes to save your time.<br/>
+            ✔️ Curated Playlists – Best video lectures & tutorials for every subject.<br/>
+            ✔️ Guidance & Tips – Strategies from toppers to help you stay ahead.<br/>
+
+            Our goal is simple – to empower our juniors and batchmates with the best resources so that you can learn smarter, not harder, and achieve academic excellence effortlessly.<br/>
+
+            🚀 Join us in this journey to the top!<br/>
+        </p>
+    
+    </div>
   )
 }
 
